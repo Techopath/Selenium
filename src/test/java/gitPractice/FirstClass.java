06dc5d7fc9eb7ff11546e1c31d1742586fd8f0c0
Muhammad <musadam28@gmail.com>
2020-04-07 17:54:34 -0400

    first change

---
+++ src/test/java/gitPractice/FirstClass.java
@@ -0,0 +1,8 @@
+package gitPractice;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("We added this class to develop and then merge it to master");
+
+    }
+}
